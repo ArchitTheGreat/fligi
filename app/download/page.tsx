d3a429c01def85403f5b8ca812ce079cebac956a
Archit <86586285+ArchitTheGreat@users.noreply.github.com>
2025-04-01 21:16:04 +0530

    c5

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -48,17 +48,46 @@ export default function Web() {
                 <li className="mb-4">
                     <a href="https://architbhunia.netlify.app/" className="hover:underline">My Portofolio</a>
                 </li>
-                
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Twitter</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Facebook</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Contact Us</a>
+                </li>
             </ul>
         </div>
-        
+        <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Privacy Policy</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Licensing</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                </li>
+            </ul>
+        </div>
         <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
                     <a href="#" className="hover:underline">iOS</a>
                 </li>
-                
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Android</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">Windows</a>
+                </li>
+                <li className="mb-4">
+                    <a href="#" className="hover:underline">MacOS</a>
+                </li>
             </ul>
         </div>
     </div>
